1d329ceee578af9de6b1a5fb2238332a11e22e5a
Dungntt2003 <nguyenthuydung55555@gmail.com>
2024-04-01 08:33:11 +0700

    Add comment to function

--- code/ulities.py
+++ code/ulities.py
@@ -1,6 +1,7 @@
 # this file has functions that get name of city, famous place in each city
 def getPath(path):
     # viết hàm ở đây
+    # path là mảng, yêu cầu là lần lượt in ra các thành phố mà thời gian in cách nhau khoảng lâu 30s
     print("Path")
 
 
