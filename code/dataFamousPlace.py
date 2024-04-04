dce2d1bc4d6dfff181490a5bd5684c4e110b3c6c
LinhBP <ygchsohcy@gmail.com>
2024-04-05 00:09:44 +0700

    Update dataFamousPlace and def goBack

--- code/dataFamousPlace.py
+++ code/dataFamousPlace.py
@@ -300,182 +300,172 @@ famousPlace = [
         ]
     },
     {
-        "name" : "",
-        "tourist_spots" : [
+        "name": "Lạng Sơn",
+        "tourist_spots": [
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Thành cổ Lạng Sơn",
+                "address": "Phường Tam Thanh, Thành phố Lạng Sơn",
+                "description": "Công trình phòng thủ từ thời kỳ phong kiến"
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Đền Mẫu Tổ",
+                "address": "Xã Hoàng Đồng, Huyện Cao Lộc",
+                "description": "Xây dựng để tưởng nhớ công lao những vị anh hùng dân tộc"
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Đèo Mẫu Sơn",
+                "address": "Phường Tam Thanh, Thành phố Lạng Sơn",
+                "description": "Địa điểm thích hợp cho những người yêu thích leo núi và khám phá"
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Hang Pác Bó và hang Bản Sen",
+                "address": "Xã Hữu Lễ, Huyện Bình Gia",
+                "description": "Hang động tự nhiên vơi hệ thống động thực vật, các hình thù đá phong phú"
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
-            },
-        ]
-    },
-    {
-        "name" : "",
-        "tourist_spots" : [
-            {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Chợ Đồng Đăng",
+                "address": "Xã Tràng Định, Huyện Cao Lộc",
+                "description": "Chợ biên giới khu vực cửa khẩu quốc tế Đồng Đăng, khám phá và mua sắm các sản phẩm đặc sản, nông sản hàng hóa từ Trung Quốc và các nước khác"
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
-            },
-            {
-                "name" : "",
-                "address" : "",
-                "description" : ""
-            },
-            {
-                "name" : "",
-                "address" : "",
-                "description" : ""
-            },
-            {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Suối Mỡ",
+                "address": "Xã Văn Quan, Huyện Văn Quan",
+                "description": "Địa điểm du lịch tự nhiên với suối nước trong xanh, rừng cây xanh tươi và không gian mát mẻ"
             },
         ]
     },
     {
-        "name" : "",
-        "tourist_spots" : [
+        "name": "Hà Giang",
+        "tourist_spots": [
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Đèo Mã Pí Lèng",
+                "address": "Xã Pá Mỳ, Huyện Mèo Vạc",
+                "description": "Một trong những con đèo nổi tiếng và đẹp nhất Việt Nam, có thể tận hưởng khung cảnh đồng cỏ xanh mướt và dòng sông Nho Quế chảy xuyên qua vùng núi đá chồi lồi"
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Cung đường Đồng Văn",
+                "address": "Huyện Đồng Văn",
+                "description": "Đi qua những vùng đá đặc trưng của Đồng Văn, trên đường đi có thể ngắm nhìn các cánh đồng lúa bậc thang, các ngôi nhà cổ và thưởng ngoạn cảnh quan núi rừng hùng vĩ"
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Vườn quốc gia Đồng Văn",
+                "address": "Xã Phó Bảng, Huyện Đồng Văn",
+                "description": "Khu bảo tồn thiên nhiên với động thực vật phong phú. Có thể tham quan, đi bộ hoặc leo núi để khám phá các khu vực đẹp và hoang sơ"
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Chợ đầu mùa",
+                "address": "Xã Mèo Vạc, Huyện Mèo Vạc",
+                "description": "Diễn ra vào thứ bảy hàng tuần. Là nơi giao lưu văn hóa, thương mại và trưng bày các sản phẩm đặc sản của các dân tộc vùng cao"
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
-            },
-        ]
-    },
-    {
-        "name" : "",
-        "tourist_spots" : [
-            {
-                "name" : "",
-                "address" : "",
-                "description" : ""
-            },
-            {
-                "name" : "",
-                "address" : "",
-                "description" : ""
-            },
-            {
-                "name" : "",
-                "address" : "",
-                "description" : ""
-            },
-            {
-                "name" : "",
-                "address" : "",
-                "description" : ""
-            },
-            {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Lũng Cú",
+                "address": "Xã Cao Mã Pờ, Huyện Đồng Văn",
+                "description": "Nằm giữa các dãy núi hùng vĩ, có cảnh quan đẹp, rừng nguyên sinh và các làng bản của người dân tộc"
             },
         ]
     },
     {
-        "name" : "",
-        "tourist_spots" : [
+        "name": "Lào Cai",
+        "tourist_spots": [
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Sa Pa",
+                "address": "Xã Sa Pa, Huyện Sa Pa",
+                "description": "Thị trấn nằm ở vùng núi cao, nổi tiếng với cảnh quan thiên nhiên tuyệt đẹp và văn hóa đa dạng của dân tộc thiểu số. Du khách có thể tham quan các rừng núi, thác nước, rừng tre và thăm các bản làng dân tộc."
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Nhà thờ đá Sa Pa",
+                "address": "Xã Sa Pa, Huyện Sa Pa",
+                "description": "Là một công trình kiến trúc độc đáo, mang phong cách Gothic châu Âu. Đây là một điểm đến thu hút du khách với kiến trúc đẹp và không gian yên bình."
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Bắc Hà",
+                "address": "Huyện Bắc Hà",
+                "description": "Bắc Hà là một thị trấn nằm trong vùng núi cao của Lào Cai. Du khách có thể tham quan chợ Bắc Hà, nơi diễn ra các hoạt động thương mại và trao đổi văn hóa của các dân tộc thiểu số."
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Thác Bạc",
+                "address": "Xã Cam Cọn, Huyện Bảo Thắng",
+                "description": "Thác Bạc là một thác nước nằm cách trung tâm thành phố Lào Cai khoảng 12km. Thác nước cao và đẹp, cùng với một khu vực xung quanh tươi tốt, tạo thành một điểm đến lý tưởng cho việc thư giãn và tắm mát trong những ngày hè nóng."
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Hồ Tản Viên",
+                "address": "Thung lũng Mường Hoa, Xã Hầu Thào, Huyện Sa Pa",
+                "description": "Hồ Tản Viên là một hồ nước tự nhiên nằm tại thung lũng Mường Hoa, gần thị trấn Sa Pa. Hồ có khung cảnh hữu tình, được bao quanh bởi những cánh đồng lúa bậc thang và ngọn núi hùng vĩ."
+            },
+            {
+                "name": "Thung lũng Mường Hoa",
+                "address": "Xã Hầu Thào, Huyện Sa Pa",
+                "description": "Thung lũng Mường Hoa là một vùng đồng cỏ xanh mướt, nằm giữa những ngọn núi đá và các bản làng của người dân tộc. Du khách có thể tận hưởng cảnh quan đẹp và khám phá văn hóa đặc trưng của các dân tộc thiểu số."
             },
         ]
     },
     {
-        "name" : "",
-        "tourist_spots" : [
+        "name": "Lai Châu",
+        "tourist_spots": [
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Hồ Than Uyên",
+                "address": "Xã Hồ Bắc, Huyện Than Uyên",
+                "description": "Với không gian hữu tình, nước hồ trong xanh và cảnh quan núi rừng xung quanh, đây là một điểm đến lý tưởng để thư giãn và tận hưởng thiên nhiên."
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Khu di tích Trung Thuỷ",
+                "address": "Thôn Trung Thuỷ, Xã Tân Uyên, Huyện Tam Đường",
+                "description": "Đây là nơi lưu giữ các tàn tích của một ngôi làng cổ của người Mông, với những căn nhà gỗ truyền thống và ruộng bậc thang."
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Chợ Sìn Hồ",
+                "address": "Thị trấn Sìn Hồ",
+                "description": "Chợ thu hút du khách bởi sự đa dạng về văn hóa và truyền thống của các dân tộc thiểu số trong khu vực."
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Thác Tiên Sơn",
+                "address": "Xã Làng Mô, Huyện Tam Đường",
+                "description": "Đây là một thác nước cao và đẹp, tạo thành một bức tranh thiên nhiên tuyệt đẹp. Du khách có thể tắm mát và thưởng ngoạn cảnh quan xung quanh."
             },
             {
-                "name" : "",
-                "address" : "",
-                "description" : ""
+                "name": "Núi Pu Sam Cáp",
+                "address": "Xã Tả Phìn, Huyện Mường Tè",
+                "description": "Điểm đến này thu hút du khách với khung cảnh núi rừng hùng vĩ và cơ hội leo núi để tận hưởng toàn cảnh vùng núi."
+            },
+            {
+                "name": "Thung lũng Mường Lò",
+                "address": "Xã Nậm Lịch, Huyện Mường Tè",
+                "description": "Thung lũng Mường Lò là một thung lũng rộng lớn nằm giữa các dãy núi, tạo thành một cảnh quan tuyệt đẹp. Du khách có thể chiêm ngưỡng cánh đồng lúa bạt ngàn và thăm các làng bản dân tộc."
+            },
+        ]
+    },
+    {
+        "name": "Điện Biên",
+        "tourist_spots": [
+            {
+                "name": "Điện Biên Phủ",
+                "address": "Phường Mường Thanh, Thành phố Điện Biên Phủ",
+                "description": "Điện Biên Phủ là nơi diễn ra trận chiến lịch sử trong cuộc kháng chiến chống lại quân đội Pháp vào năm 1954. Du khách có thể thăm quan các di tích chiến tranh, như đền A1, đền A6, đền D1 và Bunker de Castries."
+            },
+            {
+                "name": "Bản Phiêng Đắp",
+                "address": "Xã Mường Lói, Huyện Điện Biên Đông",
+                "description": "Điểm đến này hấp dẫn du khách bởi cảnh quan hùng vĩ của núi rừng, ruộng bậc thang và văn hóa truyền thống của dân tộc Mông."
+            },
+            {
+                "name": "Thác Mường Lò",
+                "address": "Xã Nà Nhạn, Huyện Điện Biên",
+                "description": "Với cảnh quan hoang sơ và nước thác trắng xóa, đây là một điểm đến tuyệt vời để thư giãn và tận hưởng thiên nhiên."
+            },
+            {
+                "name": "Làng nghề gỗ Nậm Nhùn",
+                "address": "Huyện Tuần Giáo, Tỉnh Điện Biên",
+                "description": "Du khách có thể thăm quan các cơ sở sản xuất gỗ truyền thống và khám phá nghệ thuật chế tác gỗ của người dân địa phương."
+            },
+            {
+                "name": "Hồ Nậm Chua",
+                "address": "Xã Nậm Chua, Huyện Mường Chà",
+                "description": "Với không gian yên bình và môi trường tự nhiên trong lành, đây là một điểm đến thích hợp cho du khách muốn thả bóng cá hoặc tắm mát."
+            },
+            {
+                "name": "Rừng núi Pa Thom",
+                "address": "Huyện Điện Biên Đông",
+                "description": "Điểm đến này có cảnh quan rừng núi hoang sơ, hệ động thực vật phong phú và là nơi lý tưởng để thực hiện các hoạt động trekking và khám phá thiên nhiên."
             },
         ]
     },
--- code/ulities.py
+++ code/ulities.py
@@ -9,10 +9,25 @@ def printFamousPlace(city):
      #viết hàm ở đây
     print("Famous place")
 
+def goBack(path, city):
+    if not path:
+        print("There is no previous path.")
+        return None
 
-def goBack(path,city):
-    #viết hàm ở đây
-    print("City")
+    try:
+        index_current_city = path.index(city)
+        if index_current_city > 0:
+            previous_city = path[index_current_city - 1]
+            print(f"Go back to the previous city: {previous_city}")
+            return previous_city
+        else:
+            print("This is the first city on the path.")
+            return None
+    except ValueError:
+        print(f"The city {city} is not found in the path.")
+        return None
 
-
-print("Test hàm ở đây")
+#test
+#path = ["Ha Noi", "Hai Phong", "Da Nang", "Nha Trang", "Ho Chi Minh"]
+#current_city = "Da Nang"
+#previous_city = goBack(path, current_city)
